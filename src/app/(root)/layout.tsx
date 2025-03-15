045bce9b994c167199db0e63a769e6f00a2dbe9d
Shashank <113260615+shashankaz@users.noreply.github.com>
2025-03-15 16:23:06 +0530

    Update HomeLayout component to enhance layout and add footer with author link

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 interface HomeLayoutProps {
   children: React.ReactNode;
 }
 
 const HomeLayout = ({ children }: HomeLayoutProps) => {
-  return <div className="px-4 sm:px-6 md:px-8 lg:px-10">{children}</div>;
+  return (
+    <div className="px-4 sm:px-6 md:px-8 lg:px-10 min-h-screen flex flex-col justify-between">
+      {children}
+      <p className="text-center py-4">
+        Made with ❤️ by{" "}
+        <Link
+          href="https://x.com/shashankaz"
+          target="_blank"
+          className="hover:underline"
+        >
+          Shashank
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default HomeLayout;
