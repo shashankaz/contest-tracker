01447717d2a41974e5bcc0bf9a6437649cc436ac
Shashank <113260615+shashankaz@users.noreply.github.com>
2025-04-20 22:30:45 +0530

    Update Loading component styles for improved visual consistency

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,32 +2,33 @@ const Loading = () => {
   return (
     <div>
       <div className="flex items-center justify-between h-20">
-        <div className="h-10 w-64 bg-gray-600 animate-pulse rounded-lg"></div>
+        <div className="h-10 w-64 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-lg"></div>
         <div className="hidden md:flex items-center gap-3">
-          <div className="h-10 w-40 bg-gray-600 animate-pulse rounded-lg"></div>
-          <div className="h-10 w-40 bg-gray-600 animate-pulse rounded-lg"></div>
-          <div className="h-10 w-16 bg-gray-600 animate-pulse rounded-lg"></div>
-          <div className="h-10 w-10 bg-gray-600 animate-pulse rounded-lg"></div>
+          <div className="h-10 w-40 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-lg"></div>
+          <div className="h-10 w-40 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-lg"></div>
+          <div className="h-10 w-40 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-lg"></div>
+          <div className="h-10 w-16 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-lg"></div>
+          <div className="h-10 w-10 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-lg"></div>
         </div>
-        <div className="flex md:hidden h-8 w-8 bg-gray-600 animate-pulse rounded-md"></div>
+        <div className="flex md:hidden h-8 w-8 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-md"></div>
       </div>
 
       <div>
-        <div className="h-10 bg-gray-600 animate-pulse rounded-md mt-4"></div>
+        <div className="h-10 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-md mt-4"></div>
 
         {Array(10)
           .fill(0)
           .map((_, index) => (
             <div
               key={index}
-              className="h-8 bg-gray-600 animate-pulse my-1 rounded-md"
+              className="h-8 bg-gray-200 dark:bg-gray-600 animate-pulse my-1 rounded-md"
             ></div>
           ))}
       </div>
 
       <div className="flex justify-end gap-3 mt-4">
-        <div className="h-10 w-24 bg-gray-600 animate-pulse rounded-lg"></div>
-        <div className="h-10 w-24 bg-gray-600 animate-pulse rounded-lg"></div>
+        <div className="h-10 w-24 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-lg"></div>
+        <div className="h-10 w-24 bg-gray-200 dark:bg-gray-600 animate-pulse rounded-lg"></div>
       </div>
     </div>
   );
